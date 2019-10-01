0ff5427280d7e70a9598b443023da74f5d909f6c
jinyoung0323 <kjycjb686@naver.com>
2019-10-01 11:21:48 +0900

    12

--- jsbasic.js
+++ jsbasic.js
@@ -24,3 +24,55 @@ var t = Number('500');
 document.write(n, "<br>");
 document.write(t, "<br>");
 document.write("<br>");
+
+// 논리형
+var b = true;
+var b2 = 10 >= 100;
+var k = Boolean("hello");
+var l = Boolean(0);
+var m = Boolean(null);
+
+document.write(b, "<br>");
+document.write(b2, "<br>");
+document.write(k, "<br>");
+document.write(l, "<br>");
+document.write(m, "<br>");
+document.write("<hr>");
+
+// null & undefined
+var u;
+var h = "hello";
+h = null;
+
+document.write(u, "<br>");
+document.write(h, "<br>");
+document.write("<hr>");
+
+// typeof
+document.write(typeof s, "<br>");
+document.write(typeof n, "<br>");
+document.write(typeof b, "<br>");
+document.write(typeof u, "<br>");
+document.write(typeof h, "<br>");
+document.write("<hr>");
+
+// 문자열 결합 연산
+var str = "<table border = '1'>";
+str += "<tr>";
+str += "<td>1</td><td>2</td><td>3</td>";
+str += "</tr>";
+str += "</table>";
+
+document.write(str, "<br>");
+document.write("<hr>");
+
+// 비교 연산
+var k = 10, m = "10";
+
+document.write(k == 10, "<br>");
+document.write(m == 10, "<br>");
+document.write("<hr>");
+
+document.write(k === 10, "<br>");
+document.write(m === 10, "<br>");
+document.write("<hr>");
