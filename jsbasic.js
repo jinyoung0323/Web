e6bb2cbca1f8bf2e4e3e8079042fb71a3f4ccdae
jinyoung0323 <kjycjb686@naver.com>
2019-10-01 10:13:57 +0900

    1

--- jsbasic.js
+++ jsbasic.js
@@ -6,3 +6,13 @@ box = 100;
 document.write(box, "<br>");
 console.log(box);
 document.write("<hr>");
+
+// 문자형
+var s = "JavaScript";
+var num = '100';
+var tag = "<h3>JavaScript String</h3>";
+
+document.write(s, "<br>");
+document.write(num, "<br>");
+document.write(tag, "<br>");
+document.write("<hr>");
